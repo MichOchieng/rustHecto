ba2a69be6c61614ebfed611df2dccc5d2914546f
Mich Ochieng <michochieng@gmail.com>
2022-04-24 15:54:29 -0700

    Using match in main block, fixes

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,28 @@ fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-
-        if c.is_control(){
-            println!("{:?}",b);
-        } else {
-            println!("{:?} ({})\r",b,c);
-        }
-        if b == ctrl('q') {
-            break;
-        }
+    // If wrapped b is Ok do something otherwise Err
+       match b {
+        //    Avoiding into_raw_mode error
+        //    Handling output
+           Ok(b) => {
+               let c = b as char;
+               if c.is_control() {
+                   println!("{:?} \r",b);
+               } else {
+                   println!("{:?} ({})\r",b,c);
+               }
+               if b == ctrl('q') {
+                   break;
+               }
+           }
+           Err(err) => die(err)
+       }
     }
 }
 // Error handling: prints error and exits program
 fn die(e: std::io::Error) {
-    panic!(e)
+    panic!("{}",e)
 }
 
 // Used to exit program when control is pressed in combination with passed value
