3bcebf462be0716e447bb84a3c908c91899b9f18
Mich Ochieng <michochieng@gmail.com>
2022-05-01 21:04:45 -0700

    Reads keys instead of bytes

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
+    for key in io::stdin().keys() {
     // If wrapped b is Ok do something otherwise Err
-       match b {
+       match key {
         //    Avoiding into_raw_mode error
         //    Handling output
-           Ok(b) => {
-               let c = b as char;
-               if c.is_control() {
-                   println!("{:?} \r",b);
-               } else {
-                   println!("{:?} ({})\r",b,c);
-               }
-               if b == ctrl('q') {
-                   break;
-               }
-           }
-           Err(err) => die(err)
+           Ok(key) => match key {
+               Key::Char(c) => {
+                   if c.is_control(){
+                       println!("{:?}\r",c as u8);
+                   }else {
+                    println!("{:?} ({})\r",c as u8,c);
+                   }
+                }
+               Key::Ctrl('q') => break,
+               _ => println!("{:?}\r",key),
+           },
+           Err(err) => die(err),
        }
     }
 }
+
 // Error handling: prints error and exits program
 fn die(e: std::io::Error) {
     panic!("{}",e)
-}
-
-// Used to exit program when control is pressed in combination with passed value
-fn ctrl(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
 }
