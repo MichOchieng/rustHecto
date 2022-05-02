1103554fc900c8b37420b36013d8bcb709e43d22
Mich Ochieng <michochieng@gmail.com>
2022-05-01 21:13:31 -0700

    Removed editor code

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,10 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+mod editor;
+use editor::Editor;
+
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-    // If wrapped b is Ok do something otherwise Err
-       match key {
-        //    Avoiding into_raw_mode error
-        //    Handling output
-           Ok(key) => match key {
-               Key::Char(c) => {
-                   if c.is_control(){
-                       println!("{:?}\r",c as u8);
-                   }else {
-                    println!("{:?} ({})\r",c as u8,c);
-                   }
-                }
-               Key::Ctrl('q') => break,
-               _ => println!("{:?}\r",key),
-           },
-           Err(err) => die(err),
-       }
-    }
+    let editor = Editor{};
+    editor.run();
 }
 
-// Error handling: prints error and exits program
-fn die(e: std::io::Error) {
-    panic!("{}",e)
-}
+
